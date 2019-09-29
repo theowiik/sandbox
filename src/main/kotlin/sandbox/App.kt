b3c271722ce761ba08bba60cdb31a92c48c591f5
theowiik <theo.le.wiik@gmail.com>
2019-09-29 20:51:57 +0200

    Basic main app

--- src/main/kotlin/sandbox/App.kt
+++ src/main/kotlin/sandbox/App.kt
@@ -1,7 +1,27 @@
 package sandbox
 
+import sandbox.model.IModel
+import sandbox.model.ModelImpl
+import sandbox.view.IView
+import sandbox.view.ViewImpl
+
 class App {
+  private val model: IModel
+  private val view: IView
+
+  init {
+    model = ModelImpl()
+    view = ViewImpl(model)
+  }
+
+  fun start() {
+    for (i in 0..100) {
+      view.repaint()
+    }
+  }
 }
 
 fun main() {
+  val app = App()
+  app.start()
 }
