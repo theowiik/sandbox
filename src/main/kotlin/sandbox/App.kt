0d3e9026ac598af1f10d07d762f14ced2fe064e2
theowiik <theo.le.wiik@gmail.com>
2019-09-29 15:32:27 +0200

    Cleaned up App.kt

--- src/main/kotlin/sandbox/App.kt
+++ src/main/kotlin/sandbox/App.kt
@@ -1,12 +1,7 @@
 package sandbox
 
 class App {
-  val greeting: String
-    get() {
-      return "Hello world."
-    }
 }
 
-fun main(args: Array<String>) {
-  println(App().greeting)
+fun main() {
 }
