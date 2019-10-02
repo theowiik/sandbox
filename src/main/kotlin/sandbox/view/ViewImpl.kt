47b903f41291f81b9885f6e9ba9f81402256b012
theowiik <theo.le.wiik@gmail.com>
2019-10-02 21:59:29 +0200

    Removed render repaint implementation

--- src/main/kotlin/sandbox/view/ViewImpl.kt
+++ src/main/kotlin/sandbox/view/ViewImpl.kt
@@ -14,17 +14,5 @@ class ViewImpl(
 ) : IView {
 
   override fun repaint() {
-    for (row in model.getWorld()) {
-      for (pixel in row) {
-        if (pixel.hasMaterial()) {
-          print(pixel.content?.name()?.toCharArray()?.first())
-        } else {
-          print(" ")
-        }
-      }
-      println("") // New line
-    }
-
-    println("-----")
   }
 }
