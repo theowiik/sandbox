599818a670eac7f1bae21cc27c1ec2c0423cbb4c
theowiik <theo.le.wiik@gmail.com>
2019-10-02 21:59:57 +0200

    moved logic from model class to new world class

--- src/main/kotlin/sandbox/model/ModelImpl.kt
+++ src/main/kotlin/sandbox/model/ModelImpl.kt
@@ -9,35 +9,9 @@ import sandbox.model.materials.rigidbodies.Sand
  * @see IModel
  */
 class ModelImpl() : IModel {
-  private val world = mutableListOf<MutableList<Pixel>>()
+  private val world: World = World()
 
-  init {
-    initWorld()
-  }
-
-  /**
-   * Initializes the world.
-   */
-  private fun initWorld() {
-    val width = 20
-    val height = 10
-
-    for (h in 0..height) {
-      val tempRow = mutableListOf<Pixel>()
-
-      for (w in 0..width) {
-        tempRow.add(Pixel())
-
-        if ((0..10).random() == 1) {
-          tempRow[w].content = Sand()
-        }
-      }
-
-      world.add(tempRow)
-    }
-  }
-
-  override fun getWorld(): List<List<Pixel>> {
+  override fun getWorld(): World {
     return world
   }
 }
