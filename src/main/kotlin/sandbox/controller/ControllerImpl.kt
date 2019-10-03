7045553a0626447b383488545075c113b753335b
theowiik <theo.le.wiik@gmail.com>
2019-10-03 21:56:43 +0200

    added kotlin doc for controller impl

--- src/main/kotlin/sandbox/controller/ControllerImpl.kt
+++ src/main/kotlin/sandbox/controller/ControllerImpl.kt
@@ -4,6 +4,11 @@ import javafx.stage.Stage
 import sandbox.model.IModel
 import sandbox.view.IView
 
+/**
+ * The ControllerImpl class is a implementation of the IController interface.
+ *
+ * @see IController
+ */
 class ControllerImpl(
     private val model: IModel,
     private val view: IView,
