fb85846709d757b2b42d28ceb872dcc1833c42a2
theowiik <theo.le.wiik@gmail.com>
2019-10-03 21:56:08 +0200

    Added color to sand class

--- src/main/kotlin/sandbox/model/materials/rigidbodies/Sand.kt
+++ src/main/kotlin/sandbox/model/materials/rigidbodies/Sand.kt
@@ -1,5 +1,8 @@
 package sandbox.model.materials.rigidbodies
 
+import sandbox.model.colors.ColorImpl
+import sandbox.model.colors.IColor
+
 /**
  * The Sand class is a type of rigid body.
  *
@@ -7,4 +10,5 @@ package sandbox.model.materials.rigidbodies
  */
 class Sand() : RigidBody() {
   override val name: String = "Sand"
+  override val color: IColor = ColorImpl(0xc2, 0xb2, 0x80)
 }
