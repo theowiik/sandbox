cab9b14ebfacb22e0a1da6b0fafed53144d03f44
theowiik <theo.le.wiik@gmail.com>
2019-10-05 16:46:56 +0200

    load root scene in sandbox pane

--- src/main/kotlin/sandbox/controller/components/SandboxPane.kt
+++ src/main/kotlin/sandbox/controller/components/SandboxPane.kt
@@ -1,15 +1,24 @@
 package sandbox.controller.components
 
+import javafx.fxml.FXMLLoader
 import javafx.scene.layout.AnchorPane
 import sandbox.controller.IController
 import sandbox.model.IModel
+import java.net.URL
 
 /**
  * The main window.
  */
 class SandboxPane(
-    val model: IModel,
-    val controller: IController
+    private val model: IModel,
+    private val controller: IController
 ) : AnchorPane() {
-  val sandboxPane: SandboxPane = SandboxPane(model, controller)
+  private val sandboxPane: SandboxPane = SandboxPane(model, controller)
+
+  init {
+    val scenePath: URL = URL("root.fxml")
+    val loader = FXMLLoader(scenePath)
+    loader.setRoot(this)
+    loader.setController(this)
+  }
 }
