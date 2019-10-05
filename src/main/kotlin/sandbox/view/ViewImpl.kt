d53985c0261d6c82228ab654fb9bb23ef0ee146d
theowiik <theo.le.wiik@gmail.com>
2019-10-05 21:59:00 +0200

    Added service for converting a matrix of colors to a fx image

--- src/main/kotlin/sandbox/view/ViewImpl.kt
+++ src/main/kotlin/sandbox/view/ViewImpl.kt
@@ -1,7 +1,13 @@
 package sandbox.view
 
 import javafx.scene.canvas.GraphicsContext
+import javafx.scene.image.Image
+import javafx.scene.image.PixelWriter
+import javafx.scene.image.WritableImage
+import javafx.scene.paint.Color
 import sandbox.model.IModel
+import sandbox.service.ColorService
+import sandbox.service.ImageService
 
 /**
  * Implementation of the IView interface.
@@ -13,5 +19,8 @@ class ViewImpl(
     override var graphicsContext: GraphicsContext? = null
 ) : IView {
   override fun repaint() {
+    graphicsContext?.clearRect(0.0, 0.0, 20.0, 20.0)
+    val fxImage: Image = ImageService.colorsToFXImage(model.getWorldColors())
+    graphicsContext?.drawImage(fxImage, 0.0, 0.0)
   }
 }
