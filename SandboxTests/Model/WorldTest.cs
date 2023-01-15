aa47aedc4eb07d7e7f82e8014f294e4f96a507e8
theowiik <theo.le.wiik@gmail.com>
2023-01-15 16:53:12 +0100

    Add world test

---
+++ SandboxTests/Model/WorldTest.cs
@@ -0,0 +1,12 @@
+using Sandbox.Model;
+
+namespace SandboxTests.Model;
+
+public sealed class WorldTest
+{
+    [Test]
+    public void SampleTest()
+    {
+        var world = new World();
+    }
+}
